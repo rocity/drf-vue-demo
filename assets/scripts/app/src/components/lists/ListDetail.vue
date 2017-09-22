46fd8efba1af55944a38887c8420c1749a2dd12d
Kevin Paul Kalis <kevinpaulkalis@gmail.com>
2017-09-22 16:32:39 +0800

    Render list detail

--- assets/scripts/app/src/components/lists/ListDetail.vue
+++ assets/scripts/app/src/components/lists/ListDetail.vue
@@ -1,3 +1,30 @@
 <template>
-  <h1>List Detail</h1>
+  <div class="container">
+    <h1>List Detail</h1>
+    <h3>{{ list.title }}</h3>
+    <hr>
+    <h3>Items</h3>
+    <ul>
+      <li v-for='item in list.items'>{{ item.content }}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+  import resources from '@/resources'
+
+  export default {
+    data () {
+      return {
+        list: {}
+      }
+    },
+    created () {
+      resources.fetchListDetails(this.$route.params.list_id).then(response => {
+        console.info('response--->>>', response.body)
+        this.list = response.body
+      })
+    }
+  }
+
+</script>
--- assets/scripts/app/src/resources/index.js
+++ assets/scripts/app/src/resources/index.js
@@ -1,8 +1,13 @@
 import API from '@/constants/index'
 import Vue from 'vue'
 
+const apiRoute = `${API.API_DOMAIN}${API.API_URL}`
+
 export default {
   fetchLists () {
-    return Vue.http.get(`${API.API_DOMAIN}${API.API_URL}lists`)
+    return Vue.http.get(`${apiRoute}lists`)
+  },
+  fetchListDetails (id) {
+    return Vue.http.get(`${apiRoute}lists/${id}`)
   }
 }
